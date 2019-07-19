87f6f52efaed5e24e27a7344a247e3bc8eef1bf5
llxgg <491894345@qq.com>
2019-07-19 10:47:08 +0800

    login版本1.0.0

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-
-
 Vue.use(Router);
 
-export default new Router({
-  routes: [
+// @ 代表 src这一层目录
+import Login from '@/components/login/login.vue';
 
+export default new Router({
+  mode:"history",
+  routes: [
+    {
+      name:'login',
+      path:'/login',
+      component:Login
+    }
   ],
 });
